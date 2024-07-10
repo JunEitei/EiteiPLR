b16f16d9e4de55418c7ea342233d9bae638348a0
damao <dadada.maomaomao@gmail.com>
2024-07-10 17:58:06 +0900

    Example-SPM

---
+++ Example-SPM/Example-SPM/SceneDelegate.swift
@@ -0,0 +1,19 @@
+import UIKit
+import EiteiPLR
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+
+        window?.rootViewController = ViewController()
+
+        window?.makeKeyAndVisible()
+    }
+
+}
